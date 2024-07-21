a786cb30c43a669b11fcbe0a5fdec329c87b6146
Statistician <pallalasr4@gmail.com>
2024-07-21 08:33:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
+  });
+</script>
